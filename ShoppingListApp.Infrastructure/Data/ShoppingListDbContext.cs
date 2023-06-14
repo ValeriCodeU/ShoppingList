3921a39cfeb2dcf243ce3a815848ead9f2c3c0ae
ValeriCodeU <valeri.yanev@icloud.com>
2023-06-14 09:23:07 +0300

    Updated project

--- ShoppingListApp.Infrastructure/Data/ShoppingListDbContext.cs
+++ ShoppingListApp.Infrastructure/Data/ShoppingListDbContext.cs
@@ -42,13 +42,13 @@ namespace ShoppingListApp.Infrastructure.Data
 
         private void SeedUserRoles(ModelBuilder builder)
         {
-            //Admin
+            //Admin user
 
             builder.Entity<IdentityUserRole<Guid>>().HasData(
                 new IdentityUserRole<Guid>() { RoleId = new Guid("b3bf89bc-b80a-4b24-9f4b-9a90bb4fe652"), UserId = new Guid("48787569-f841-4832-8528-1f503a8427cf") }
                 );
 
-            //Customer
+            //Customer user
 
             builder.Entity<IdentityUserRole<Guid>>().HasData(
                new IdentityUserRole<Guid>() { RoleId = new Guid("5fb4a609-b894-4db7-a7cf-7fe80cb6d536"), UserId = new Guid("bfbcc7d7-2e7e-4d3c-b7fb-4b76f27cefe3") }
