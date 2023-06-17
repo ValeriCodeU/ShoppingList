8ed7815e7693e6f427aba27d330270f3c86bf01f
ValeriCodeU <valeri.yanev@icloud.com>
2023-06-17 22:32:52 +0300

    Added string length attribute to view model

--- ShoppingListApp.Core/Models/Category/CategoryDetailsViewModel.cs
+++ ShoppingListApp.Core/Models/Category/CategoryDetailsViewModel.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using static ShoppingListApp.Core.Constants.ViewModelConstants.Category;
 
 namespace ShoppingListApp.Core.Models.Category
 {
     public class CategoryDetailsViewModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(MaxCategoryName, MinimumLength = MinCategoryName)]
 
         public string Name { get; set; } = null!;
     }
